e5c02d82e5460b5556395b317b54faa5acf6ffd7
wowon <bestofsw@naver.com>
2025-08-16 23:00:03 +0900

    Button taps

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -293,7 +293,7 @@ static void MX_GPIO_Init(void)
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
-/* USER CODE BEGIN+ MX_GPIO_Init_2 */
+/* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
 
--- Core/Src/mytask.c
+++ Core/Src/mytask.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "cmsis_os.h"
 #include "main.h"
 #include "FreeRTOS.h"
@@ -6,59 +7,106 @@
 #include "queue.h"
 #include "semphr.h"
 
-void task(void *pvParameters);
+#define DEBOUNCE_TIME_MS       50
+#define TAP_THRESHOLD_MS       250
 
-SemaphoreHandle_t xButtonSemaphore;
+void btnTask(void *pvParameters);
 
-int global_i=0;
-uint16_t GPIO_Pin_BTN;
+
+typedef struct
+{
+  uint8_t ch;
+  uint32_t pre_time;
+  uint8_t tap_count;
+  uint32_t last_tap_time;
+} button_obj_t;
+
+button_obj_t buttonObj[3];
+
+void buttonObjectCreate(button_obj_t *p_obj, uint8_t ch)
+{
+  p_obj->ch = ch;
+  p_obj->pre_time = HAL_GetTick();
+  p_obj->tap_count = 0;
+  p_obj->last_tap_time = 0;
+}
 
 void myTask()
 {
-
-  //xButtonSemaphore  = xSemaphoreCreateBinary(); // Binary Semaphore
-  xButtonSemaphore  = xSemaphoreCreateCounting(3,0); // Counting Semaphore
-
-
-  xTaskCreate(task, "Task1", 256, "TASK1", 3, NULL);
+  buttonObjectCreate(&buttonObj[0], 0); // button 1
+  buttonObjectCreate(&buttonObj[1], 1); // button 2
+  buttonObjectCreate(&buttonObj[2], 2); // button 3
+  xTaskCreate(btnTask, "btnTask", 256, NULL, 3, NULL);
 }
 
-void task(void *pvParameters)
+void buttonObjectProcess(button_obj_t *p_obj)
 {
-  int local_i = 0;
+  uint32_t now = HAL_GetTick();
 
-  while(1)
+  if (p_obj->tap_count > 0)
   {
-    if (xSemaphoreTake(xButtonSemaphore, portMAX_DELAY) == pdTRUE)
+    if (now - p_obj->last_tap_time > TAP_THRESHOLD_MS)
     {
-      printf("BTN PIN , Counting , local_i : %x,  %lu,  %d \n",GPIO_Pin_BTN, uxSemaphoreGetCount(xButtonSemaphore), local_i);
-      local_i++;
+      switch (p_obj->tap_count)
+      {
+        case 1:
+          printf("ONE TAP ch:%d\n", p_obj->ch);
+          break;
+        case 2:
+          printf("DOUBLE TAP ch:%d\n", p_obj->ch);
+          break;
+        case 3:
+          printf("TRIPLE TAP ch:%d\n", p_obj->ch);
+          break;
+        default:
+          printf("TAP COUNT ERROR : %d \n",p_obj->tap_count);
+          break;
+      }
+      p_obj->tap_count = 0;
     }
-    vTaskDelay(100);
   }
 }
 
+void btnTask(void *pvParameters)
+{
+  printf("btn task start\n");
 
-#define DEBOUNCE_TIME_MS 50
-uint32_t debounce_time;
+  while(1)
+  {
+    for(int i=0; i<3; i++)
+    {
+      buttonObjectProcess(&buttonObj[i]);
+    }
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-
-  //Debounce
   uint32_t now = HAL_GetTick();
-  if (now-debounce_time < DEBOUNCE_TIME_MS)
+  uint16_t btn_ID;
+
+  if(GPIO_Pin == BTN1_Pin)
+  {
+    btn_ID = 0;
+  }
+  else if(GPIO_Pin == BTN2_Pin)
+  {
+    btn_ID = 1;
+  }
+  else if(GPIO_Pin == BTN3_Pin)
+  {
+    btn_ID = 2;
+  }
+  else
     return;
-  debounce_time = now;
 
+  // Debounce check
+  if (now - buttonObj[btn_ID].pre_time < DEBOUNCE_TIME_MS)
+    return;
+  buttonObj[btn_ID].pre_time = now;
+  //
 
-  GPIO_Pin_BTN = GPIO_Pin;
-
-  xSemaphoreGiveFromISR(xButtonSemaphore, &xHigherPriorityTaskWoken);
-  xSemaphoreGiveFromISR(xButtonSemaphore, &xHigherPriorityTaskWoken);
-  xSemaphoreGiveFromISR(xButtonSemaphore, &xHigherPriorityTaskWoken);
-
-  portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+  buttonObj[btn_ID].tap_count++;
+  buttonObj[btn_ID].last_tap_time = now;
 }
-
